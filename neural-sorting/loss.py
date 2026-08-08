c617333f7a1975beaf51443e2accc6f2b06789b9
cecinuga <cecinuga.tdb@gmail.com>
2026-08-08 15:25:08 +0200

    rollback, disorder v2 sucks

--- neural-sorting/loss.py
+++ neural-sorting/loss.py
@@ -47,29 +47,6 @@ def disorder_test(row: int, length_upto: list[int], seed: float) -> None:
                 raise e
 
 
-
-"""Loss function for disorder V2: without sum reduction"""
-
-def disorder_v2(x: np.ndarray) -> np.ndarray:
-    assert len(x) > 0
-
-    """Base case: a vector with lenth 1 has no disorder."""
-    if len(x) == 1: return 0
-
-    return np.abs(np.diff(x))
-
-
-def disorder_v2_grad(x: np.ndarray, grad_output: np.ndarray) -> np.ndarray:
-    g = np.zeros_like(x)
-    if len(x) > 1:
-        s = np.sign(np.diff(x))
-        weighted = grad_output * s
-        g[:-1] -= weighted
-        g[1:] += weighted
-
-    return g
-
-
 """Tiny-Torch compliant implementation of loss function (must be moved on framework repo as soon as possible)"""
 
 class DisorderLossBackward(Function):
@@ -97,26 +74,3 @@ class DisorderLoss(Loss):
         out._grad_fn = self.grad_fn(predictions, targets)
         return out
     
-"""Disorder V2 Tiny-Torch classes"""
-
-class DisorderV2LossBackward(Function):
-    @override
-    def apply(self, grad_output: Tensor) -> tuple[Tensor, ...]:
-        pred, targ = self.saved_tensors
-
-        grad_pred = disorder_v2_grad(pred.data, grad_output.data)
-        grad_targ = -disorder_v2_grad(targ.data, grad_output.data)
-
-        return Tensor(grad_pred), Tensor(grad_targ)
-
-class DisorderV2Loss(Loss):
-    grad_fn:type[Function] = DisorderV2LossBackward
-
-    @override
-    def forward(self, predictions: Tensor, targets: Tensor) -> Tensor:
-        pred_disorder = disorder_v2(predictions)
-        targ_disorder = disorder_v2(targets)
-
-        out = Tensor(pred_disorder - targ_disorder)
-        out._grad_fn = self.grad_fn(predictions, targets)
-        return out
